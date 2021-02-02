f7c52fce5931d57041fd0930b8ce3fc5af8d1452
Michał <michalkolarzyk@gmail.com>
2021-02-02 22:37:39 +0100

    Minor changes

---
+++ Services/ShedulerServices/IScheduleService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.ShedulerServices
+{
+    public interface IScheduleService
+    {
+        Task Start();
+        Task Stop();
+    }
+}
---
+++ Services/ShedulerServices/SchedulerService.cs
@@ -0,0 +1,28 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.ShedulerServices
+{
+    public class SchedulerService : IScheduleService
+    {
+        StdSchedulerFactory factory = new StdSchedulerFactory();
+        IScheduler _scheduler;
+        Dictionary<Action, ITrigger> _tasks;
+
+
+        public async Task Start()
+        {
+            JobBuilder.Create<BaseJob>().SetJobData(new JobDataMap());
+        }
+
+        public async Task Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
